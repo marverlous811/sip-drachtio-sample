77c2b66a7ad37237bcc4d5e0d37e726630f43a8a
shadowWalker <hieu811@gmail.com>
2024-06-17 15:32:52 +0700

    init project

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+import path from 'path'
+import * as dotenv from 'dotenv'
+
+const envPath = path.join(process.cwd(), '.env')
+dotenv.config({
+  path: envPath,
+  override: true,
+})
+
+export const ENV = process.env.ENV || 'develop'
+
+export const DRACHTIO_CONFIG = {
+  host: process.env.DRACHTIO_HOST || '127.0.0.1',
+  port: parseInt(process.env.DRACHTIO_PORT || '9022'),
+  secret: process.env.DRACHTIO_SECRET || '',
+}
+
+export const MRF_CONFIG = {
+  address: process.env.MRF_ADDRESS || '127.0.0.1',
+  port: parseInt(process.env.MRF_PORT || '8021'),
+  secret: process.env.MRF_SECRET || '',
+}
